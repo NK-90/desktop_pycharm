890fbfaeabc032707b654a0dfdd340f56de4e58c
NK-90 <62362977+NK-90@users.noreply.github.com>
2020-05-25 23:25:56 +0900

    기능개발 ver3

--- 프로그래머스/기능개발.py
+++ 프로그래머스/기능개발.py
@@ -1,22 +1,31 @@
-pro = [93,30,55]
-up  = [1,30,5]
+pro = [93,30,40,55,30]
+up  = [1,30,30,5,3]
 
 
 
 
+Temp  = []
 temp = []
-ans = []
+ans   = []
 
 for idx,val in enumerate(pro):
     n=0
     while val < 100:
        val += up[idx]
        n +=1
-    temp.append(n)
-    print(idx,temp)
-    if temp[0] < temp[-1]:
-        ans.append(len(temp) - 1)
-        temp = [temp[-1]]
-print(ans)
+    Temp.append(n)
+
+print(Temp)
+k = Temp[0]
+for idx,val in enumerate(Temp):
+     temp.append(Temp.pop(0))
+     if idx != 0:
+       k = Temp[0]
+     print(idx,k,Temp,temp,val)
+     if k < val:
+         print(idx,temp)
+         ans.append(len(temp))
+         temp= []
+
 
 
