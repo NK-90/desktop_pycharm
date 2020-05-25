5a5f2701125760026b252ed85b3552362de64295
NK-90 <62362977+NK-90@users.noreply.github.com>
2020-05-26 05:13:43 +0900

    기능개발 ver5(pass)

--- 프로그래머스/기능개발.py
+++ 프로그래머스/기능개발.py
@@ -3,33 +3,33 @@ up  = [1,30,30,5,30,1,30,30]
 
 
 
+def solution(pro,up):
+    Temp  = []
+    temp = []
+    ans   = []
 
-Temp  = []
-temp = []
-ans   = []
-
-for idx,val in enumerate(pro):
-    n=0
-    while val < 100:
-       val += up[idx]
-       n +=1
-    Temp.append(n)
-
-
-k = Temp.pop(0)
-temp.append(k)
-
-for i in range(len(Temp)):
-    if k >= Temp[0]:
-        temp.append(Temp.pop(0))
-    elif k < Temp[0]:
-        ans.append((len(temp)))
-        temp.clear()
-        k = Temp.pop(0)
-        temp.append(k)
-
-ans.append(len(temp))
+    for idx,val in enumerate(pro):
+        n=0
+        while val < 100:
+           val += up[idx]
+           n +=1
+        Temp.append(n)
 
 
+    k = Temp.pop(0)
+    temp.append(k)
 
+    for i in range(len(Temp)):
+        if k >= Temp[0]:
+            temp.append(Temp.pop(0))
+        elif k < Temp[0]:
+            ans.append((len(temp)))
+            temp.clear()
+            k = Temp.pop(0)
+            temp.append(k)
+
+    ans.append(len(temp))
+
+
+    return ans
 
