c067859ed0c13e509db80fa01adb1f61ccefec77
NK-90 <62362977+NK-90@users.noreply.github.com>
2020-05-27 03:29:07 +0900

    프린터 ver3(pass)

--- 프로그래머스/프린터.py
+++ 프로그래머스/프린터.py
@@ -1,27 +1,21 @@
-List = [1, 1, 9, 1, 1, 1]
-location = 0
+# List = [1, 1, 9, 1, 1, 1]
+# Zist = [0, 1, 2, 3, 4, 5]
+# location = 2
 
 
-def solution(List,location):
+def solution(List,locatino):
     Zist = [x for x in range(len(List))]
     Aist = []
 
-
-
-    for i in range(10000):
-
-        for j in List:
+    for j in range(1000):
+        if List[0] == max(List):
+            List.pop(0)
+            Aist.append(Zist.pop(0))
+            if len(List) == 0:
+                break
+        else:
             List.append(List.pop(0))
             Zist.append(Zist.pop(0))
-            if List[0] == max(List):
-                List.pop(0)
-                Aist.append(Zist.pop(0))
 
-        if len(Zist) == 0:
-            break
-    print(Aist)
-    print(Aist.index(location)+1)
-    return Aist.index(location)+1
+    return Aist.index(location) + 1
 
-
-solution(List,location)
