e793f26e364500508b7981de0378d800ac4a198d
NK-90 <62362977+NK-90@users.noreply.github.com>
2020-05-29 10:01:21 +0900

    레이저 ver8

--- 프로그래머스/레이저.py
+++ 프로그래머스/레이저.py
@@ -1,20 +1,17 @@
 
-arr = '()(((()())(())()))(())'
-
-
 
 def solution(arr):
+    arr = arr.replace('()', 'L')
     Aist = list(arr)
     k,ans = 0,0
     for i in range(len(Aist)):
-           if Aist[i] == '(' and Aist[i+1] != ')':
-                   k +=1
-                   ans +=1
-           if Aist[i] == ')' and Aist[i-1] != '(':
-                   k -=1
-           if Aist[i] == '(' and Aist[i+1] == ')' and k != 0:
-                   ans +=k
+           if Aist[i] == '(':
+               k +=1
+               ans =+1
+           if Aist[i] == "L":
+               ans+=k
+           if Aist[i] == ')':
+               k -=1
 
     return ans
-
 
